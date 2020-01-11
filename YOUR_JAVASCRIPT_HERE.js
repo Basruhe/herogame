ac87f4f098b59bb83ba34306a824e73af998558f
Basruhe <sb.ruhe@gmail.com>
2020-01-11 13:40:48 +0100

    Added rest function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -14,7 +14,26 @@ const hero = {
 
 //Tests
 console.log(Object.keys(hero))
-console.log(Object.keys(hero.weapon))
 console.log(hero.inventory)
 
+//Creating placeholder variable
+const person = hero
+console.log(Object.keys(person))
 
+//Resting function
+const rest = function(person) {
+  console.log (hero.health + " Health before function logic")
+  
+  if (hero.health < 10) {
+    person.health=10
+    console.log ("Health set to " + hero.health)
+    return (person)
+  }
+  if (hero.health === 10) {
+    console.log ("You are already at full health. No need to rest")
+    //window.alert ("You are already at full health. There is no need to rest")
+    return (person)
+  }
+}
+
+rest()
