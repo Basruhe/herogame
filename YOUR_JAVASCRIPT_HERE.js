cc0ab411007ab80e14251d1be3102b4d2f0165b6
Basruhe <sb.ruhe@gmail.com>
2020-01-11 19:13:02 +0100

    Trying to get event on image click to work

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,6 +1,6 @@
 // Write your JS here
 
-
+//Creating hero object
 const hero = {
   name: "Bas",
   heroic: true,
@@ -12,53 +12,95 @@ const hero = {
   }
 };
 
-//Tests
+
+//Object Tests
 console.log(Object.keys(hero));
 console.log(typeof(hero.inventory));
 
 //Creating placeholder variable
 const person = hero
 console.log(Object.keys(person));
+console.log (hero.health);
 
-//Resting function
-const rest = function(person) {
-  console.log (person.health + " Health before function logic")
+// RESTING FUNCTION
+rest = function(person) {
+  console.log (`There is ${person.health} health before function logic`)
   
   if (person.health < 10) {
-    person.health=10
+    person.health = 10
     console.log (`Health reset to ${person.health} health`)
-    return(person)
     }
     if (person.health === 10) {
-    console.log (`You are already at full health. No need to rest`)
+    console.log ("You are already at full health. No need to rest");
     //window.alert ("You are already at full health. There is no need to rest")
     
   }
   return(person)
 };
 
-// Testing event listener for  image click:
-const innPress = document.getElementById("inn")
+console.log (person.health);
+
+// Testing event listener for  image click (works)
+const innPress = document.getElementById("inn");
 innPress.addEventListener("click", function() {
-  window.alert("this works!");
+  window.alert("This works")
 });
 
 // Trying to run a function on image click (does not work)
-// const innPress = document.getElementById("inn")
+// const innPress = document.getElementById("inn");
 // innPress.addEventListener("click", testfunction());
-
-// function testfunction() {
-//   window.alert ("ok");
+// const testfunction = function () {
+//   window.alert ("Does this work?");
 // };
 
 
 
 
 
-// const pickUpItem = function() {
-//   console.log ("pickup function")
-// };
-// const equipWeapon = function() {
-//   console.log ("equip function")
-// };
+// PICKUP ITEM
+
+//Weapon pickup:
+
+console.log (`Inventory contains ${person.inventory} before pickup`);
+
+
+const pickUpItem = function(person, weapon) {
+  console.log ("Weapon added:")
+  person.inventory.push (weapon);
+  console.log(person.inventory);
+};
+const weapon = "dagger";
+
+console.log (`Inventory contains ${person.inventory} after pickup`);
+ 
+//Testing event listener for image click (works) 
+// const weaponPress = document.getElementById("dagger");
+// weaponPress.addEventListener("click", function() {
+//   window.alert("This works")
+// });
+
+// Trying to run the function on  image click (does not work)
+const weaponPress = document.getElementById("dagger");
+weaponPress.addEventListener("click", pickUpItem(person, weapon));
+
+
+
+
+
+
+// const adding = function (a, b,) {
+//   return a + b
+// }
+// console.log (adding (1,2));
+
+
+
+
+
+
+
+
+const equipWeapon = function() {
+
+};
 
