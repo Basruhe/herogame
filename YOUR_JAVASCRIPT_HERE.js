5ae59177b8de1ed0357e39061717e2b517ecda3e
Basruhe <sb.ruhe@gmail.com>
2020-01-11 12:10:10 +0100

    Added game object

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,20 @@
 // Write your JS here
+
+
+const hero = {
+  name: "Bas",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "Sword",
+    damage: 2
+  }
+}
+
+//Tests
+console.log(Object.keys(hero))
+console.log(Object.keys(hero.weapon))
+console.log(hero.inventory)
+
+
